6b3b83057022f2e2b4b85a64868816b96ae33400
Yuchao Zhang <418121364@qq.com>
2017-08-24 14:27:23 +0800

    Update run_tts.sh

--- egs/slt_arctic/local/run_tts.sh
+++ egs/slt_arctic/local/run_tts.sh
@@ -45,7 +45,6 @@ if [ $stage -le 0 ]; then
   if $kaldi_format; then
     [ -f $voicenet_dir/misc/scripts/kaldi_path.sh ] && . $voicenet_dir/misc/scripts/kaldi_path.sh;
     for x in train test val; do
-        cmp_context_mat[:, 0] = cmp_mat[:, 0]
     {
       convert-binary-to-matrix "ark:${raw}/prepared_label/label_scp/${x}.scp" "ark,scp:${data}/${x}_label.ark,${data}/${x}_label.scp"
       convert-binary-to-matrix "ark:${raw}/prepared_cmp/param_scp/${x}.scp" "ark,scp:${data}/${x}_param.ark,${data}/${x}_param.scp"
